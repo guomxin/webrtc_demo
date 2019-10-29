cedb65e435dc7f6eb3e956538972b56a54f9bee9
guomxin <guomaoxin@foxmail.com>
2019-10-29 11:59:38 +0800

    version 0.9

---
+++ public/js/client.js
@@ -0,0 +1,215 @@
+'use strict';
+
+//set constraints
+const mediaStreamConstraints = {
+	video: true,
+};
+
+// Set up to exchange only video.
+const offerOptions = {
+	offerToReceiveVideo: 1,
+};
+
+// Define connection to signal server
+let socket = io.connect('https://10.30.21.53');
+const room = 1;
+
+socket.on("joined", (room) => {
+	trace(`Joined room: ${room}`);
+});
+
+socket.on("full", (room) => {
+	trace(`Room: ${room} is full.`);
+});
+
+// Define peer connections, streams and video elements.
+const localVideo = document.getElementById('localVideo');
+const remoteVideo = document.getElementById('remoteVideo');
+let localStream;
+let peerConnection;
+
+// Define action buttons.
+const startButton = document.getElementById('startButton');
+const callButton = document.getElementById('callButton');
+const hangupButton = document.getElementById('hangupButton');
+
+// Set up initial action buttons status: disable call and hangup.
+callButton.disabled = true;
+hangupButton.disabled = true;
+
+// Add click event handlers for buttons.
+startButton.addEventListener('click', startAction);
+callButton.addEventListener('click', callAction);
+hangupButton.addEventListener('click', hangupAction);
+
+// Sets the MediaStream as the video element src.
+function gotLocalMediaStream(mediaStream) {
+	localVideo.srcObject = mediaStream;
+	localStream = mediaStream;
+	trace('Received local stream.');
+	callButton.disabled = false;  // Enable call button.
+}
+
+// Handles error by logging a message to the console.
+function handleLocalMediaStreamError(error) {
+	trace(`navigator.getUserMedia error: ${error.toString()}.`);
+}
+
+function startAction() {
+	startButton.disabled = true;
+	navigator.mediaDevices.getUserMedia(mediaStreamConstraints)
+		.then(gotLocalMediaStream).catch(handleLocalMediaStreamError);
+	trace("Requesting local stream.")
+
+	socket.emit("join", room);
+
+	const servers = null;  // Allows for RTC server configuration.
+	// Create peer connections and add behavior.
+	peerConnection = new RTCPeerConnection(servers);
+	trace('Created peer connection object.');
+	peerConnection.addEventListener('icecandidate', handleConnection);
+	peerConnection.addEventListener('addstream', gotRemoteMediaStream);
+}
+
+// Logs error when setting session description fails.
+function setSessionDescriptionError(error) {
+	trace(`Failed to create session description: ${error.toString()}.`);
+}
+
+// Logs success when setting session description.
+function setLocalDescriptionSuccess() {
+	trace('Finished setting local description.');
+}
+function setRemoteDescriptionSuccess() {
+	trace('Finished setting remote description.');
+}
+
+function createdOffer(description) {
+	trace(`[Caller] Offer from local PeerConnection:\n${description.sdp}`);
+
+	trace('[Caller] PeerConnection setLocalDescription start.');
+	peerConnection.setLocalDescription(description)
+		.then(setLocalDescriptionSuccess)
+		.catch(setSessionDescriptionError);
+
+	// Send to signal server
+	socket.emit('caller-send-sdp', room, description);
+}
+
+socket.on("receiver-recv-sdp", (description) => {
+	trace(`[Receiver] Received sdp: ${description.sdp}`);
+	trace('[Receiver] PeerConnection setRemoteDescription start.');
+	peerConnection.setRemoteDescription(description)
+		.then(setRemoteDescriptionSuccess)
+		.catch(setSessionDescriptionError);
+
+	trace('[Receiver] PeerConnection createAnswer start.');
+	peerConnection.createAnswer()
+		.then(createdAnswer)
+		.catch(setSessionDescriptionError);
+});
+
+// Logs answer to offer creation and sets peer connection session descriptions.
+function createdAnswer(description) {
+	trace(`[Receiver] Answer from PeerConnection:\n${description.sdp}.`);
+	trace('[Receiver] PeerConnection setLocalDescription start.');
+	peerConnection.setLocalDescription(description)
+		.then(setLocalDescriptionSuccess)
+		.catch(setSessionDescriptionError);
+
+	// Send to signal server
+	socket.emit('receiver-send-sdp', room, description);
+}
+
+socket.on("caller-recv-sdp", (description) => {
+	trace(`[Caller] Received sdp: ${description.sdp}`);
+	trace('[Caller] PeerConnection setRemoteDescription start.');
+	peerConnection.setRemoteDescription(description)
+		.then(setRemoteDescriptionSuccess)
+		.catch(setSessionDescriptionError);
+});
+
+// Handles call button action: creates peer connection.
+function callAction() {
+	callButton.disabled = true;
+	hangupButton.disabled = false;
+	trace('Starting call.');
+
+	if (peerConnection == null) {
+		const servers = null;  // Allows for RTC server configuration.
+		// Create peer connections and add behavior.
+		peerConnection = new RTCPeerConnection(servers);
+		trace('Created peer connection object.');
+		peerConnection.addEventListener('icecandidate', handleConnection);
+		peerConnection.addEventListener('addstream', gotRemoteMediaStream);
+	}
+
+
+	// Get local media stream tracks.
+	const videoTracks = localStream.getVideoTracks();
+	const audioTracks = localStream.getAudioTracks();
+	if (videoTracks.length > 0) {
+		trace(`Using video device: ${videoTracks[0].label}.`);
+	}
+	if (audioTracks.length > 0) {
+		trace(`Using audio device: ${audioTracks[0].label}.`);
+	}
+
+	peerConnection.addStream(localStream);
+	trace('Added local stream to peerConnection.');
+
+	trace('CreateOffer start.');
+	peerConnection.createOffer(offerOptions)
+		.then(createdOffer).catch(setSessionDescriptionError);
+}
+
+// Connects with new peer candidate.
+function handleConnection(event) {
+	const iceCandidate = event.candidate;
+	if (iceCandidate) {
+		trace(`[send-ice] ${iceCandidate}`);
+		socket.emit('send-ice', room, iceCandidate);
+	}
+}
+
+socket.on('recv-ice', (iceCandidate) => {
+	trace(`[recv-ice] ${iceCandidate}`);
+	peerConnection.addIceCandidate(iceCandidate)
+		.then(handleConnectionSuccess)
+		.catch(handleConnectionFailure);
+});
+
+// Logs that the connection succeeded.
+function handleConnectionSuccess() {
+	trace('AddIceCandidate success.');
+};
+
+// Logs that the connection failed.
+function handleConnectionFailure(error) {
+	trace(`Failed to add ICE Candidate:\n` + `${error.toString()}.`);
+}
+
+// Handles remote MediaStream success by adding it as the remoteVideo src.
+function gotRemoteMediaStream(event) {
+	const mediaStream = event.stream;
+	remoteVideo.srcObject = mediaStream;
+	trace('Peer connection received remote stream.');
+}
+
+// Handles hangup action: ends up call, closes connections and resets peers.
+function hangupAction() {
+	peerConnection.close();
+	peerConnection = null;
+	hangupButton.disabled = true;
+	callButton.disabled = false;
+	trace('Ending call.');
+}
+
+// Define helper functions
+
+// Logs an action (text) and the time when it happened on the console.
+function trace(text) {
+	text = text.trim();
+	const now = (window.performance.now() / 1000).toFixed(3);
+	console.log(now, text);
+}
---
+++ server.js
@@ -0,0 +1,89 @@
+'use strict'
+
+//var log4js = require('log4js');
+var http = require('http');
+var https = require('https');
+var fs = require('fs');
+var socketIo = require('socket.io');
+
+var express = require('express');
+var serveIndex = require('serve-index');
+
+var USERCOUNT = 2;
+
+// log4js.configure({
+//     appenders: {
+//         file: {
+//             type: 'file',
+//             filename: 'app.log',
+//             layout: {
+//                 type: 'pattern',
+//                 pattern: '%r %p - %m',
+//             }
+//         }
+//     },
+//     categories: {
+//        default: {
+//           appenders: ['file'],
+//           level: 'debug'
+//        }
+//     }
+// });
+
+// var logger = log4js.getLogger();
+
+var app = express();
+app.use(serveIndex('./public'));
+app.use(express.static('./public'));
+
+//http server
+var http_server = http.createServer(app);
+http_server.listen(8080, '0.0.0.0');
+
+// https server
+var options = {
+	key : fs.readFileSync('./public/cert/xingm.pc.key'),
+	cert: fs.readFileSync('./public/cert/xingm.pc.crt')
+}
+var https_server = https.createServer(options, app);
+https_server.listen(443, '0.0.0.0');
+var io = socketIo.listen(https_server);
+
+io.sockets.on('connection', (socket)=> {
+
+	socket.on('send-ice', (room, data)=>{
+		console.log(`[send-ice]: ${data}`);
+		socket.to(room).emit('recv-ice', data);
+	});
+
+	socket.on('caller-send-sdp', (room, data)=>{
+		console.log(`[caller-send-sdp]: ${data}`);
+		socket.to(room).emit('receiver-recv-sdp', data);
+	});
+
+	socket.on('receiver-send-sdp', (room, data)=>{
+		console.log(`[receiver-send-sdp]: ${data}`);
+		socket.to(room).emit('caller-recv-sdp', data);
+	});
+
+	socket.on('join', (room)=>{
+		var myRoom = io.sockets.adapter.rooms[room]; 
+		var users = (myRoom)? Object.keys(myRoom.sockets).length : 0;
+		console.log('the user number of room (' + room + ') is: ' + users);
+		if(users < USERCOUNT) {
+		    socket.join(room);
+			socket.emit('joined', room); //发给自己
+		    //io.in(room).emit('message', room, msg); //发给房间内的所有人
+		    //socket.emit('message', room, msg); //发给房间内的所有人
+		    //socket.broadcast.emit('message', room, msg); //发给房间内的所有人
+		
+		} else {
+			socket.emit('full', room);
+            var msg = 'room:' + room + ' is full';
+			console.log(msg);
+		}
+	});
+});
+
+
+
